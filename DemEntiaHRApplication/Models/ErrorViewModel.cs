a4e196fcc5bf55f315e8e4dab9e497b2b85044ad
Dev-Niko Snellman <dev-niko.snellman@ALUENIMI3.LOCAL>
2017-10-09 11:57:06 +0300

    Add project files.

---
+++ DemEntiaHRApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemEntiaHRApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
