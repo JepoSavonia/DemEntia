3bca52dc810357761f120d3aa2c79453d84e64c7
Snellu <31610969+Snellu@users.noreply.github.com>
2017-10-23 10:04:53 +0300

    DemEntia pohja

---
+++ DemEntiaHRApplication/Controllers/DesiteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemEntiaHRApplication.Controllers
+{
+    public class DesiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult ADUser()
+        {
+            return View();
+        }
+
+        public IActionResult HRUser()
+        {
+            return View();
+        }
+    }
+}
--- DemEntiaHRApplication/Startup.cs
+++ DemEntiaHRApplication/Startup.cs
@@ -43,7 +43,7 @@ namespace DemEntiaHRApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Desite}/{action=Index}/{id?}");
             });
         }
     }
