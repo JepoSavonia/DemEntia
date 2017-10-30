04afc9295dc0880a5c582535fc0d05510de8edbc
Maetomias <31610783+Maetomias@users.noreply.github.com>
2017-10-30 12:04:06 +0200

    Admin sivu, edit user, js +css

--- DemEntiaHRApplication/wwwroot/js/site.js
+++ DemEntiaHRApplication/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+
+    $(".editUserBtn").click(function () {
+        $(".shadow").removeClass("hidden");
+        $(".editUserPage").removeClass("hidden");
+    });
+
+    $(".closeEditUserPage").click(function () {
+        $(".shadow").addClass("hidden");
+        $(".editUserPage").addClass("hidden");
+    });
+});
