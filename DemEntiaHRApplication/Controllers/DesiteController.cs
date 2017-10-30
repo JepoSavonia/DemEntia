f628c30f2019a71cd43c23a95af6725ccab1f093
Snellu <31610969+Snellu@users.noreply.github.com>
2017-10-30 11:23:49 +0200

    Kirjautuminen

--- DemEntiaHRApplication/Controllers/DesiteController.cs
+++ DemEntiaHRApplication/Controllers/DesiteController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DemEntiaHRApplication.Models;
+using Savonia.AdManagement;
 
 namespace DemEntiaHRApplication.Controllers
 {
@@ -11,6 +13,16 @@ namespace DemEntiaHRApplication.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserViewModel user)
+        {
+            BetterAdManager adManager = new BetterAdManager(new AccountManagementConfig { Domain = "ALUENIMI3.LOCAL", Container = "OU=Users,OU=DE,DC=ALUENIMI3,DC=LOCAL", Username = user.UserName, Password = user.Password });
+
+            SavoniaUserObject userObject = adManager.FindUser(user.UserName);
+
+            return View(user);
         }
 
         public IActionResult Admin()
